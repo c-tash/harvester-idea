b1d372338f993b6a13faaff60fb4b5a980b09e88
c-tash <ctash23@gmail.com>
2015-04-05 20:07:07 +0300

    Updated all of the features for the harvester.

--- trash/src/DBProcedure.java
+++
@@ -1,6 +0,0 @@
-public interface DBProcedure {
-	final static String db_connect_string = "jdbc:sqlserver://localhost:1433;";
-	final static String dbName = "databaseName=HarvestingSchedule";
-	final static String dbUser = "QueryLogin";
-	final static String dbPssw = "fvpQas26yPAkggCF";
-}
